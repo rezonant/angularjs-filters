b53f73f998bc63a70c15528bcff163e7436d851a
Liam <liam@aether>
2015-06-26 22:12:45 -0400

    Karma uses NPM deps; Remove stale static deps

--- karma.conf.js
+++ karma.conf.js
@@ -15,9 +15,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'lib/jquery/**/*.js',
-      'lib/angularjs/**/*.js',
-      'lib/angularjs-mocks/**/*.js',
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
       'filters.js',
       'test/tests.js',
       'test/browser_tests.js'
